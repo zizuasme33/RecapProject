30020528e9240da9774aa15f58c072895438033f
zizuasme33 <82923966+zizuasme33@users.noreply.github.com>
2021-06-30 21:27:53 -0400

    Us102 has been completed (#2)
    combine all 3 commit by squashing it

---
+++ src/test/java/UserStory102.java
@@ -0,0 +1,8 @@
+public class UserStory102 {
+
+    // lost of code has been written here
+    // ready to push this branch to remote
+    // and open pull request so I can start collaborating
+    // so my team member can comment review my code and approve or request for more changes
+    // more changes added as requested
+}
