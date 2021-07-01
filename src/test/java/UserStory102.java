bff8ad05e927365952174212d9512e76bfa8b06b
Zebib Teweldebrhan <zizu_ss@yahoo.com>
2021-06-30 21:19:35 -0400

    User story 102 has been completed

--- src/test/java/UserStory102.java
+++ src/test/java/UserStory102.java
@@ -1,2 +1,7 @@
 public class UserStory102 {
+
+    // lost of code has been written here
+    // ready to push this branch to remote
+    // and open pull request so I can start collaborating
+    // so my team member can comment review my code and approve or request for more changes
 }
